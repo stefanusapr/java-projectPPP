53bf8bf7d442927979c704336c430361a7731728
stefanusapr <a.p.stefanus@gmail.com>
2018-05-09 14:05:26 +0700

    versi 0

---
+++ src/Collectional/Iterator.java
@@ -0,0 +1,10 @@
+package Collectional;
+
+/**
+ *
+ * @author db2admin067
+ */
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
---
+++ src/Collectional/Party.java
@@ -0,0 +1,10 @@
+package Collectional;
+
+/**
+ *
+ * @author db2admin067
+ */
+public interface Party {
+    public void semuaBeraksi();
+    Iterator getIterator();
+}
